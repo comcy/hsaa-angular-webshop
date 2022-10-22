7a3680c7b9354a8cc88f80a7c7e39f55871b5269
Christian Silfang <christian.silfang@gmail.com>
2022-10-23 00:29:51 +0200

    Update product-list.component.ts

--- src/webshop-frontend/projects/webshop-app/src/app/components/product-list/product-list.component.ts
+++ src/webshop-frontend/projects/webshop-app/src/app/components/product-list/product-list.component.ts
@@ -11,14 +11,14 @@ import {
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-  public productList: Product[] = [];
+  public productList: Product[] = []; // Liste von Produkten
 
   constructor(private productsService: ProductHttpService) {}
 
   ngOnInit(): void {
     this.productsService
       .getProducts()
-      .subscribe((response: ProductResponse) => {
+      .subscribe((response: ProductResponse) => { // Subscription auf ein "Observable" vom Type "ProductResponse"
         console.log('>>> Products ', response);
         this.productList = response.content;
       });
